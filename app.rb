f3203f45336b83ab6ae6feebab1a4f1c1a10a181
Kohl Kinning <kohlkinning@Kohls-Kumquat.local>
2014-09-23 14:15:41 -0700

    almost have files working

--- app.rb
+++ app.rb
@@ -8,13 +8,5 @@ get '/' do
 end
 
 get '/todo' do
-
-    file_contents = File.read('todo.txt')
-
-    lines = file_contents.split("\n")
-
-    lines.each do |line|
-        thing, date = line.split("-")
-        puts "ToDo -  #{date}: #{thing}"
-    end
+    erb :todo
 end
