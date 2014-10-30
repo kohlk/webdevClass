32d6d69cca509aefb1d90a0955f983e6994e9143
Kohl Kinning <kohlk@lclark.edu>
2014-10-29 22:06:34 -0700

    didn't quite clean up (TodoItem.rb)

--- models/ToDoItem.rb
+++ models/ToDoItem.rb
@@ -1,12 +1,3 @@
-if ENV['DATABASE_URL']
-    ActiveRecord::Base.establish_connection(ENV['DATABASE_URL'])
-else
-    ActiveRecord::Base.establish_connection(
-        :adapter  => 'sqlite3',
-        :database => 'db/development.db',
-        :encoding => 'utf8'
-    )
-end
 class TodoItem < ActiveRecord::Base
 
 end
